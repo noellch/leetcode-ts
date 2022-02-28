791bf7b6d51d86d84760ecfed7e8033902e681b7
chiahsinglo <ausgeflippte@gmail.com>
2022-02-28 14:28:29 +0800

    0228

--- 17 - math/412.FizzBuzz.ts
+++ 17 - math/412.FizzBuzz.ts
@@ -8,19 +8,17 @@ answer[i] == i if non of the above conditions are true.
 
 //? 給定一個整數 n，返回一個長度 n 的 array。index 可以被 3 整除該值 Fizz，可以被 5 整除該值 Buzz，可同時被兩數整除，該值 FizzBuzz。
 
-var fizzBuzz = function (n) {
-    const answer = []
+var fizzBuzz = function (n: number): string[] {
+    const answer: string[] = []
 
-    for (let i = 0; i < n; i++) {
-        answer[i] = `${i + 1}`
-        if ((i + 1) % 3 === 0) answer[i] = 'Fizz'
-        if ((i + 1) % 5 === 0) answer[i] = 'Buzz'
-        if ((i + 1) % 15 === 0) answer[i] = 'FizzBuzz'
+    while (n-- > 0) {
+        answer[n] = String(n + 1)
+        if ((n + 1) % 5 === 0) answer[n] = 'Buzz'
+        if ((n + 1) % 3 === 0) answer[n] = 'Fizz'
+        if ((n + 1) % 15 === 0) answer[n] = 'FizzBuzz'
     }
 
     return answer
 }
 
 console.log(fizzBuzz(15))
-
-//* 簡單到懷疑是不是有陷阱...
