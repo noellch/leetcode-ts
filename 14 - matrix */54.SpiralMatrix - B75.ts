723d7bf4d2fb907b168bce85e92db5970f4fb0d8
chiahsinglo <ausgeflippte@gmail.com>
2022-03-05 15:13:27 +0800

    review matrix

--- 14 - matrix */54.SpiralMatrix - B75.ts
+++ 14 - matrix */54.SpiralMatrix - B75.ts
@@ -1,13 +1,14 @@
-/* Q: Given an m x n matrix, return all elements of the matrix in spiral order. */
+/* Given an m x n matrix, return all elements of the matrix in spiral order. */
 
-var spiralOrder = function (matrix) {
-    const result = []
-    // 設定上下左右的 boundary
-    let top = 0,
-        left = 0,
+function spiralOrder(matrix: number[][]): number[] {
+    // 設定 boundaries
+    let left = 0,
+        top = 0,
         right = matrix[0].length - 1,
         bottom = matrix.length - 1
-    const resultSize = matrix[0].length * matrix.length
+    const result: number[] = []
+
+    const resultSize = matrix.length * matrix[0].length
 
     // result 的長度一定會等於原本 matrix 的總元素。所以一相等就跳出迴圈。
     while (result.length < resultSize) {
@@ -15,24 +16,30 @@ var spiralOrder = function (matrix) {
         for (let i = left; i <= right && result.length < resultSize; i++) {
             result.push(matrix[top][i])
         }
+
         top++
         // right 的上到下
         for (let i = top; i <= bottom && result.length < resultSize; i++) {
             result.push(matrix[i][right])
         }
+
         right--
         // bottom 的右到左
         for (let i = right; i >= left && result.length < resultSize; i--) {
             result.push(matrix[bottom][i])
         }
+
         bottom--
         // left 的下到上
         for (let i = bottom; i >= top && result.length < resultSize; i--) {
             result.push(matrix[i][left])
         }
+
         left++
-        // 每一次 loop 完都必須將 boundary 的範圍縮小。
+
+        // 每一次 loop 後都 boundary 的範圍都會縮小
     }
+
     return result
 }
 
